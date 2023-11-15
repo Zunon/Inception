51384ee725d1f52025d2841618196034c46a529b
Khalifa AlMheiri <zunon@zunon.io>
2023-11-15 19:02:40 +0400

    finish, probably?

--- srcs/requirements/mariadb/docker_entrypoint.sh
+++ srcs/requirements/mariadb/docker_entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-sleep 10
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-    GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-    FLUSH PRIVILEGES;
-EOSQL
+sed -i "s/\$MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/" /etc/init_db.sql
+sed -i "s/\$MYSQL_USER/$MYSQL_USER/" /etc/init_db.sql
+sed -i "s/\$MYSQL_PASSWORD/$MYSQL_PASSWORD/" /etc/init_db.sql
+
+cat /etc/init_db.sql > /dev/stderr
+
+exec mariadbd --no-defaults --user=root --datadir=/var/lib/mysql --init-file=/etc/init_db.sql
---
+++ srcs/requirements/wordpress/docker_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+until mysqladmin ping -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" --silent;
+do
+  echo 'Waiting for the database server to be available...'
+  sleep 1
+done
+echo 'Database server is up and running!'
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+    cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+    sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/wordpress/wp-config.php
+    sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/wordpress/wp-config.php
+    sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/wordpress/wp-config.php
+    sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/wordpress/wp-config.php
+fi
+exec php-fpm81 -F
